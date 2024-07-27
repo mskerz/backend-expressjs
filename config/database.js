b0b67a9bb5739edbf14b446bdabc0645bb304a4b
Mskerz <mskz_wichasin@kkumail.com>
2024-07-27 17:00:59 +0700

    connect with link

--- config/database.js
+++ config/database.js
@@ -3,10 +3,15 @@ const config = require('./config.json'); // ตรวจสอบให้แน
 // const dbConfig = config.development;
 const dbConfig = config.production;
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect,
-  timezone: dbConfig.timezone,
+// const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+//   host: dbConfig.host,
+//   dialect: dbConfig.dialect,
+//   timezone: dbConfig.timezone,
+// });
+
+const sequelize = new Sequelize('mysql://root:wvbqVssiMyeFKKImWzvJsnduKZWOVCne@roundhouse.proxy.rlwy.net:43486/railway', {
+  dialect: 'mysql',
+  // options add here if needed
 });
 
 module.exports = sequelize;
